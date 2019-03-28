98d373d3d51d0cd7df5a61c475b1622429af6480
Edson Onildo <edsononildo@id.uff.br>
2019-03-27 23:13:49 -0300

    Removida funcao nao utilizada

--- src/Inflect.php
+++ src/Inflect.php
@@ -28,37 +28,6 @@ class Inflect
         '/$/'                      => "s"
     );
 
-    private static $singular = array(
-        '/(quiz)zes$/i'             => "$1",
-        '/(matr)ices$/i'            => "$1ix",
-        '/(vert|ind)ices$/i'        => "$1ex",
-        '/^(ox)en$/i'               => "$1",
-        '/(alias)es$/i'             => "$1",
-        '/(octop|vir)i$/i'          => "$1us",
-        '/(cris|ax|test)es$/i'      => "$1is",
-        '/(shoe)s$/i'               => "$1",
-        '/(o)es$/i'                 => "$1",
-        '/(bus)es$/i'               => "$1",
-        '/([m|l])ice$/i'            => "$1ouse",
-        '/(x|ch|ss|sh)es$/i'        => "$1",
-        '/(m)ovies$/i'              => "$1ovie",
-        '/(s)eries$/i'              => "$1eries",
-        '/([^aeiouy]|qu)ies$/i'     => "$1y",
-        '/([lr])ves$/i'             => "$1f",
-        '/(tive)s$/i'               => "$1",
-        '/(hive)s$/i'               => "$1",
-        '/(li|wi|kni)ves$/i'        => "$1fe",
-        '/(shea|loa|lea|thie)ves$/i'=> "$1f",
-        '/(^analy)ses$/i'           => "$1sis",
-        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i'  => "$1$2sis",
-        '/([ti])a$/i'               => "$1um",
-        '/(n)ews$/i'                => "$1ews",
-        '/(h|bl)ouses$/i'           => "$1ouse",
-        '/(corpse)s$/i'             => "$1",
-        '/(us)es$/i'                => "$1",
-        '/s$/i'                     => ""
-    );
-
     private static $irregular = array(
         'move'   => 'moves',
         'foot'   => 'feet',
@@ -90,7 +59,6 @@ class Inflect
             return $string;
         }
 
-
         // check for irregular singular forms
         foreach (self::$irregular as $pattern => $result) {
             $pattern = '/' . $pattern . '$/i';
@@ -102,36 +70,6 @@ class Inflect
 
         // check for matches using regular expressions
         foreach (self::$plural as $pattern => $result) {
-            if (preg_match($pattern, $string)) {
-                return preg_replace($pattern, $result, $string);
-            }
-        }
-
-        return $string;
-    }
-
-    /**
-     * @param $string
-     * @return null|string|string[]
-     */
-    public static function singularize($string)
-    {
-        // save some time in the case that singular and plural are the same
-        if (in_array(strtolower($string), self::$uncountable)) {
-            return $string;
-        }
-
-        // check for irregular plural forms
-        foreach (self::$irregular as $result => $pattern) {
-            $pattern = '/' . $pattern . '$/i';
-
-            if (preg_match($pattern, $string)) {
-                return preg_replace($pattern, $result, $string);
-            }
-        }
-
-        // check for matches using regular expressions
-        foreach (self::$singular as $pattern => $result) {
             if (preg_match($pattern, $string)) {
                 return preg_replace($pattern, $result, $string);
             }
