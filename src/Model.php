00692f7293f46bd1058d89c938d8e52c73dd811d
Edson Onildo <edsononildo@id.uff.br>
2019-03-27 22:53:51 -0300

    Correcao de bugs detectados pelos testes

--- src/Model.php
+++ src/Model.php
@@ -20,16 +20,18 @@ abstract class Model
 
     public static function find(string $where, array $values): array
     {
-        return self::bind(AR::execute("SELECT * FROM `".self::getTable()."` ".$where, $values));
+        $consulta = AR::execute("SELECT * FROM `".self::getTable()."` ".$where, $values);
+        return is_null($consulta) ? [] : self::bind($consulta);
     }
 
-    public function select(array $keys, string $cond = '', array $values = []): array
+    public static function select(array $keys, string $cond = '', array $values = []): array
     {
         $q = "SELECT ".implode(', ', $keys)." FROM ".self::getTable()." $cond";
-        return self::bind(AR::execute($q, $values));
+        $consulta = AR::execute($q, $values);
+        return is_null($consulta) ? [] : self::bind($consulta);
     }
 
-    public function save(): ?PDOStatement
+    public function save(): int
     {
         $this->getProperties($keys, $values);
 
@@ -39,10 +41,10 @@ abstract class Model
         }
         $q .= "?)";
 
-        return AR::execute($q, $values);
+        return AR::execute($q, $values)->rowCount();
     }
 
-    public function update(): ?PDOStatement
+    public function update(): int
     {
         $this->getProperties($keys, $values);
         $values[] = $this->id;
@@ -51,7 +53,7 @@ abstract class Model
         $q .= implode('` = ?, `', $keys);
         $q .= "` = ? WHERE `id` = ?";
 
-        return AR::execute($q, $values);
+        return AR::execute($q, $values)->rowCount();
     }
 
     public function delete(): ?PDOStatement
@@ -59,10 +61,15 @@ abstract class Model
         return AR::execute("DELETE FROM `".self::getTable()."` WHERE `id` = ?", [$this->id]);
     }
 
+    public static function deleteAll(): int
+    {
+        return AR::execute("DELETE FROM `".self::getTable()."`")->rowCount();
+    }
+
     private static function getTable(): string
     {
         $class = new ReflectionClass(get_called_class());
-        return Inflect::pluralize($class->getName());
+        return Inflect::pluralize(strtolower($class->getShortName()));
     }
 
     private function getProperties(&$keys, &$values)
